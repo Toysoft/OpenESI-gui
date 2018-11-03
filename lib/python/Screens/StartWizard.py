f8f3047458b5b828b0b4ab98fd8843ea0ce36975
openesi <openesi.eu@gmail.com>
2018-11-03 13:31:38 +0100

    restore openesi 8.0 for dinobot

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,39 +1,54 @@
-# Embedded file name: /usr/lib/enigma2/python/Screens/StartWizard.py
 from Wizard import wizardManager
 from Screens.WizardLanguage import WizardLanguage
 from Screens.VideoWizard import VideoWizard
 from Screens.Rc import Rc
 from Screens.Screen import Screen
+
 from boxbranding import getBoxType
+try:
+	from Plugins.SystemPlugins.OSDPositionSetup.overscanwizard import OverscanWizard
+except:
+	OverscanWizard = None
+
 from Components.Pixmap import Pixmap
 from Components.config import config, ConfigBoolean, configfile
+
 from LanguageSelection import LanguageWizard
-config.misc.firstrun = ConfigBoolean(default=True)
-config.misc.languageselected = ConfigBoolean(default=True)
-config.misc.videowizardenabled = ConfigBoolean(default=True)
+
+config.misc.firstrun = ConfigBoolean(default = True)
+config.misc.languageselected = ConfigBoolean(default = True)
+config.misc.videowizardenabled = ConfigBoolean(default = True)
+config.misc.do_overscanwizard = ConfigBoolean(default = OverscanWizard and config.skin.primary_skin.value == "Adrenalinnrw_Mod_RED/skin.xml")
 
 class StartWizard(WizardLanguage, Rc):
+	def __init__(self, session, silent = True, showSteps = False, neededTag = None):
+		self.xmlfile = ["startwizard.xml"]
+		WizardLanguage.__init__(self, session, showSteps = False)
+		Rc.__init__(self)
+		self["wizard"] = Pixmap()
+		self["HelpWindow"] = Pixmap()
+		self["HelpWindow"].hide()
+		#Screen.setTitle(self, _("Welcome..."))
+		Screen.setTitle(self, _("StartWizard"))
 
-    def __init__(self, session, silent = True, showSteps = False, neededTag = None):
-        self.xmlfile = ['startwizard.xml']
-        WizardLanguage.__init__(self, session, showSteps=False)
-        Rc.__init__(self)
-        self['wizard'] = Pixmap()
-        self['HelpWindow'] = Pixmap()
-        self['HelpWindow'].hide()
-        Screen.setTitle(self, _('StartWizard'))
+	def markDone(self):
+		# setup remote control, all stb have same settings except dm8000 which uses a different settings
+		if getBoxType() == 'dm8000':
+			config.misc.rcused.value = 0
+		else:
+			config.misc.rcused.value = 1
+		config.misc.rcused.save()
 
-    def markDone(self):
-        if getBoxType() == 'dm8000':
-            config.misc.rcused.value = 0
-        else:
-            config.misc.rcused.value = 1
-        config.misc.rcused.save()
-        config.misc.firstrun.value = 0
-        config.misc.firstrun.save()
-        configfile.save()
+		config.misc.firstrun.value = 0
+		config.misc.firstrun.save()
+		configfile.save()
 
 
-wizardManager.registerWizard(VideoWizard, config.misc.videowizardenabled.value, priority=0)
-wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.value, priority=2)
-wizardManager.registerWizard(StartWizard, config.misc.firstrun.value, priority=20)
+# mytest.py#L528ff - RestoreSettings
+wizardManager.registerWizard(VideoWizard, config.misc.videowizardenabled.value, priority = 0)
+wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.value, priority = 2)
+if OverscanWizard is not None:
+	wizardManager.registerWizard(OverscanWizard, config.misc.do_overscanwizard.value, priority = 10)
+wizardManager.registerWizard(StartWizard, config.misc.firstrun.value, priority = 20)
+# StartWizard calls InstallWizard
+# NetworkWizard priority = 25
